fd89ff137c37bb8dd14c417c7691da14f5f70be0
maridilo <146822054+maridilo@users.noreply.github.com>
2023-12-05 11:44:40 +0100

    commit

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 
 // Definicion de la estructura de environment
+
 class environment() {
 private:
     // Definicion de la estructura de variant
@@ -19,6 +20,17 @@ public:
         vars.insert(std::pair<std::string, variant>(name, value));
     }
     // Busqueda de simbolos
+int environment::lookup(const std::string& name) {
+    auto it = vars.find(name);
+    if (it != vars.end()) {
+        // Elemento encontrado, devolver un valor distinto de -1
+        return /* valor adecuado */;
+    } else {
+        // Elemento no encontrado, devolver -1
+        return -1;
+    }
+}
+// Busqueda de simbolos
     void lookup(std::string name) {
         auto it = vars.find(name);
         if (it != vars.end()) {
@@ -29,5 +41,14 @@ public:
             return -1;
         }
     }
+    // Gestion de errores
+    void error(std::string msg) {
+        std::cout << msg << std::endl;
+    }
+    // Mostrar el entorno
+    void show() {
+        for (auto it = vars.begin(); it != vars.end(); ++it) {
+            std::cout << it->first << " => " << it->second << std::endl;
+        }
+    }
 };
-}
