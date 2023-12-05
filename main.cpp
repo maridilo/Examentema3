95fa652a8ce963651dfae40cc112296bf827029c
maridilo <146822054+maridilo@users.noreply.github.com>
2023-12-05 11:37:16 +0100

    commit

--- main.cpp
+++ main.cpp
@@ -2,8 +2,10 @@
 #include <map>
 #include <string>
 
+// Definicion de la estructura de environment
 class environment() {
 private:
+    // Definicion de la estructura de variant
     std::map<std::string, variant> vars;
 public:
     void set(std::string name, variant value) {
@@ -12,19 +14,11 @@ public:
     variant get(std::string name) {
         return vars[name];
     }
+    // Insertar simbolos
     void insert(std::string name, variant value) {
         vars.insert(std::pair<std::string, variant>(name, value));
     }
-    int environment::get(std::string name) {}(const std::string& name) {
-        auto it = vars.find(name);
-        if (it != vars.end()) {
-            // Elemento encontrado, devolver un valor distinto de -1
-            return /* valor adecuado */;
-        } else {
-            // Elemento no encontrado, devolver -1
-            return -1;
-        }
-    }
+    // Busqueda de simbolos
     void lookup(std::string name) {
         auto it = vars.find(name);
         if (it != vars.end()) {
